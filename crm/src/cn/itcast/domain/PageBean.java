2af2e66803a8cb53f0bbf392edb717bfae6acf2c
QuietClickCode <2572417548@qq.com>
2018-08-17 17:13:59 +0800

    crm

---
+++ crm/src/cn/itcast/domain/PageBean.java
@@ -0,0 +1,14 @@
+package cn.itcast.domain;
+
+import java.util.List;
+
+public class PageBean {
+
+	private Integer currentPage;//当前页
+	private Integer totalCount;//总记录数
+	private Integer pageSize;//每页显示条数
+	private Integer totalPage;//总页数
+	private List list;//数据
+	
+	
+}
